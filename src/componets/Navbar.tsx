325fed9fdd3033d9911484704bb310fdbff750ff
unknown <arifk62901@gmail.com>
2025-03-09 13:50:38 +0600

    Dark-light mode toggle added using sadcn ui

--- src/componets/Navbar.tsx
+++ src/componets/Navbar.tsx
@@ -1,6 +1,7 @@
 
 import { Link } from 'react-router-dom'
 import img from '@/assets/to-do-list.png'
+import { ModeToggle } from './mode-toggle'
 export default function Navbar() {
   return (
     <div className='flex justify-between mt-3'>
@@ -9,6 +10,7 @@ export default function Navbar() {
         <Link to={'/'} className='text-2xl text-green-700 font-semibold'>Tasks<span className='text-red-600 text-2xl font-semibold'>Mange</span> </Link>
       </div>
       <div>
+        <ModeToggle />
         <Link to={'/'} className=' ml-10 mr-3'>Task</Link>
         <Link to={'/users'}>User</Link>
       </div>
--- src/main.tsx
+++ src/main.tsx
@@ -5,12 +5,15 @@ import { Provider } from 'react-redux'
 import { store } from './redux/store.ts'
 import { RouterProvider } from 'react-router-dom'
 import { routers } from './router/routers.tsx'
+import { ThemeProvider } from './provider/theme-provider.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
+    <ThemeProvider >
     <Provider store={store}>
 
    <RouterProvider router={routers} />
     </Provider>
+    </ThemeProvider>
   </StrictMode>,
 )
